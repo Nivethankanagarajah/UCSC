e1276801ec5b9773046ca1ee60ade6789ee9419b
shabthi <shabthij@gmail.com>
2018-11-24 14:58:18 +0530

    welcome

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Program</title>
+    <title>UCSC STUDENT WELFARE SYSTEM</title>
     <link href="<?php echo base_url()."assets/";?>css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url()."assets/";?>css/style.css" rel="stylesheet">
 	 <link rel="stylesheet" type="text/css" href="<?php echo base_url()."assets/";?>css/font-awesome.min.css" />
@@ -53,7 +53,7 @@
                   <div class="col-sm-8 padding_all">
                       <div class="header_inner_1">
                           <ul>
-                              <li><a href="blog.html"><i class="fa fa-cubes"></i> UCSC STUDENT WELFARE SYSTEM</a></li>
+                              <li><a href="#"><i class="fa fa-cubes"></i> UCSC STUDENT WELFARE SYSTEM</a></li>
 
                           </ul>
                       </div>
@@ -80,9 +80,9 @@
                               <!-- Collect the nav links, forms, and other content for toggling -->
                               <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                                   <ul class="nav navbar-nav">
-                                      <li class="active_1"><a href="index.html">HOME</a></li>
-                                      <li><a href="blog.html">SCOLARSHIPS</a></li>
-                                      <li><a href="details.html">FORUM</a></li>
+                                      <li class="active_1"><a href="#">HOME</a></li>
+                                      <li><a href="<?php echo base_url()."assets/";?>blog.html">SCOLARSHIPS</a></li>
+                                      <li><a href="<?php echo base_url()."assets/";?>details.html">FORUM</a></li>
 
                                   </ul>
                               </div>
